a7c5db3428b309dacef7dec123df8402c0f001e7
Renato Castro <renatocastro02@hotmail.com>
2020-12-05 12:56:55 -0300

    test: ensure RemoteAuthentication returns an Account on success

--- src/domains/test/mock-account.ts
+++ src/domains/test/mock-account.ts
@@ -1,9 +1,16 @@
 import faker from 'faker';
 import { AuthenticationParams } from '@/domains/usecases/authentication';
+import { AccountModel } from '../models/account-model';
 
 export const mockAuthentication = (): AuthenticationParams => {
   return {
     email: faker.internet.email(),
     password: faker.internet.password(),
+  };
+};
+
+export const mockAccountModel = (): AccountModel => {
+  return {
+    accessToken: faker.random.uuid(),
   };
 };
