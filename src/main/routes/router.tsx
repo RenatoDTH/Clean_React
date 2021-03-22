7837f743f5b14d098eac00727277819e601fea45
Renato Castro <renatocastro02@hotmail.com>
2021-03-22 14:55:18 -0300

    feat: ensure PrivateRoute renders component if tokens is not empty

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -4,12 +4,18 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { makeLogin } from '@/main/factories/pages/login/login-factory';
 import { makeSignUp } from '@/main/factories/pages/signup/signup-factory';
 import { ApiContext } from '@/presentation/contexts';
-import { setCurrentAccountAdapter } from '../adapters/current-account-adapter';
+import {
+  getCurrentAccountAdapter,
+  setCurrentAccountAdapter,
+} from '../adapters/current-account-adapter';
 
 const Router: React.FC = () => {
   return (
     <ApiContext.Provider
-      value={{ setCurrentAccount: setCurrentAccountAdapter }}
+      value={{
+        setCurrentAccount: setCurrentAccountAdapter,
+        getCurrentAccount: getCurrentAccountAdapter,
+      }}
     >
       <BrowserRouter>
         <Switch>
