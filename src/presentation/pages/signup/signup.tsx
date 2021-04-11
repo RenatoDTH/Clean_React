76c55b878d7c9790b0cc90c1f7b7b791c91d89c1
Renato Castro <renatocastro02@hotmail.com>
2021-04-11 13:50:50 -0300

    refactor: change useState to use callback

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -34,30 +34,40 @@ const SignUp: React.FC<Props> = ({ validation, addAccount }: Props) => {
     mainError: '',
   });
 
-  useEffect(() => {
+  const validate = (field: string): void => {
     const { name, email, password, passwordConfirmation } = state;
     const formData = { name, email, password, passwordConfirmation };
-    const nameError = validation.validate('name', formData);
-    const emailError = validation.validate('email', formData);
-    const passwordError = validation.validate('password', formData);
-    const passwordConfirmationError = validation.validate(
-      'passwordConfirmation',
-      formData,
-    );
 
-    setState({
-      ...state,
-      nameError,
-      emailError,
-      passwordError,
-      passwordConfirmationError,
+    setState((old) => ({
+      ...old,
+      [`${field}Error`]: validation.validate(field, formData),
+    }));
+
+    setState((old) => ({
+      ...old,
       isFormInvalid:
-        !!nameError ||
-        !!emailError ||
-        !!passwordError ||
-        !!passwordConfirmationError,
-    });
-  }, [state.name, state.email, state.password, state.passwordConfirmation]);
+        !!old.emailError ||
+        !!old.passwordError ||
+        !!old.nameError ||
+        !!old.passwordConfirmationError,
+    }));
+  };
+
+  useEffect(() => {
+    validate('name');
+  }, [state.name]);
+
+  useEffect(() => {
+    validate('email');
+  }, [state.email]);
+
+  useEffect(() => {
+    validate('password');
+  }, [state.password]);
+
+  useEffect(() => {
+    validate('passwordConfirmation');
+  }, [state.passwordConfirmation]);
 
   const handleSubmit = async (
     event: React.FormEvent<HTMLFormElement>,
