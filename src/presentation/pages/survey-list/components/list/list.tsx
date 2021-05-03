85e41ac33867326bd729c064364609596a5686d3
Renato Castro <renatocastro02@hotmail.com>
2021-05-03 13:07:23 -0300

    refactor: remove unnecessary Context

--- src/presentation/pages/survey-list/components/list/list.tsx
+++ src/presentation/pages/survey-list/components/list/list.tsx
@@ -1,18 +1,20 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { LoadSurveyList } from '@/domains/usecases';
 import {
   SurveyItem,
   SurveyItemEmpty,
-  SurveyContext,
 } from '@/presentation/pages/survey-list/components';
 import Styles from './list-styles.scss';
 
-const List: React.FC = () => {
-  const { state } = useContext(SurveyContext);
+type Props = {
+  surveys: LoadSurveyList.Model[];
+};
+
+const List: React.FC<Props> = ({ surveys }: Props) => {
   return (
     <ul className={Styles.listWrap} data-testid="survey-list">
-      {state.surveys.length ? (
-        state.surveys.map((survey: LoadSurveyList.Model) => (
+      {surveys.length ? (
+        surveys.map((survey: LoadSurveyList.Model) => (
           <SurveyItem key={survey.id} survey={survey} />
         ))
       ) : (
