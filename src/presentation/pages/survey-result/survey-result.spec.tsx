2032d816c80aacd1c5d842a95ea004841ec76735
Renato Castro <renatocastro02@hotmail.com>
2021-05-04 13:13:19 -0300

    test: ensure SurveyResult present error if LoadSurveyResult fails

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -7,14 +7,13 @@ import {
   mockAccountModel,
   mockSurveyResultModel,
 } from '@/domains/test';
+import { UnexpectedError } from '@/domains/errors';
 
 type SutTypes = {
   loadSurveyResultSpy: LoadSurveyResultSpy;
 };
 
-const makeSut = (surveyResult = mockSurveyResultModel()): SutTypes => {
-  const loadSurveyResultSpy = new LoadSurveyResultSpy();
-  loadSurveyResultSpy.surveyResult = surveyResult;
+const makeSut = (loadSurveyResultSpy = new LoadSurveyResultSpy()): SutTypes => {
   render(
     <ApiContext.Provider
       value={{
@@ -48,10 +47,12 @@ describe('SurveyResult Component', () => {
   });
 
   test('Should present SurveyResult data on success', async () => {
+    const loadSurveyResultSpy = new LoadSurveyResultSpy();
     const surveyResult = Object.assign(mockSurveyResultModel(), {
       date: new Date('2020-05-03T00:00:00'),
     });
-    makeSut(surveyResult);
+    loadSurveyResultSpy.surveyResult = surveyResult;
+    makeSut(loadSurveyResultSpy);
     await waitFor(() => screen.getByTestId('survey-result'));
 
     expect(screen.getByTestId('day')).toHaveTextContent('03');
@@ -78,5 +79,16 @@ describe('SurveyResult Component', () => {
     expect(percents[1]).toHaveTextContent(
       `${surveyResult.answers[1].percent}%`,
     );
+  });
+
+  test('Should render error on UnexpectedError', async () => {
+    const loadSurveyResultSpy = new LoadSurveyResultSpy();
+    const error = new UnexpectedError();
+    jest.spyOn(loadSurveyResultSpy, 'load').mockRejectedValueOnce(error);
+    makeSut(loadSurveyResultSpy);
+    await waitFor(() => screen.getByTestId('survey-result'));
+    expect(screen.queryByTestId('question')).not.toBeInTheDocument();
+    expect(screen.queryByTestId('loading')).not.toBeInTheDocument();
+    expect(screen.getByTestId('error')).toHaveTextContent(error.message);
   });
 });
