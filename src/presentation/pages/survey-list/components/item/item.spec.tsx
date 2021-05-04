9dce69aa159f38246d87d5927a39bbaeb467bd97
Renato Castro <renatocastro02@hotmail.com>
2021-05-04 14:02:18 -0300

    test: ensure SurveyList redirect to SurveyResult

--- src/presentation/pages/survey-list/components/item/item.spec.tsx
+++ src/presentation/pages/survey-list/components/item/item.spec.tsx
@@ -1,11 +1,25 @@
 import { mockSurveyModel } from '@/domains/test';
 import { IconName } from '@/presentation/components';
 import { SurveyItem } from '@/presentation/pages/survey-list/components';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory, MemoryHistory } from 'history';
 
-const makeSut = (survey = mockSurveyModel()): void => {
-  render(<SurveyItem survey={survey} />);
+type SutTypes = {
+  history: MemoryHistory;
+};
+
+const makeSut = (survey = mockSurveyModel()): SutTypes => {
+  const history = createMemoryHistory({ initialEntries: ['/'] });
+  render(
+    <Router history={history}>
+      <SurveyItem survey={survey} />
+    </Router>,
+  );
+  return {
+    history,
+  };
 };
 
 describe('SurveyItem Component', () => {
@@ -36,5 +50,12 @@ describe('SurveyItem Component', () => {
     expect(screen.getByTestId('day')).toHaveTextContent('03');
     expect(screen.getByTestId('month')).toHaveTextContent('mai');
     expect(screen.getByTestId('year')).toHaveTextContent('2020');
+  });
+
+  test('Should go to SurveyResult', () => {
+    const survey = mockSurveyModel();
+    const { history } = makeSut(survey);
+    fireEvent.click(screen.getByTestId('link'));
+    expect(history.location.pathname).toBe(`/surveys/${survey.id}`);
   });
 });
