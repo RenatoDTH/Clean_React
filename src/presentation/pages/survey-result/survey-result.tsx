be9701c5330d56d93ca952e4e3e4f42be3c625de
Renato Castro <renatocastro02@hotmail.com>
2021-05-09 16:21:06 -0300

    feat: ensure SurveyResult prevent multiple answer click

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -34,6 +34,10 @@ const SurveyResult: React.FC<Props> = ({
   });
 
   const onAnswer = (answer: string): void => {
+    if (state.isLoading) {
+      return;
+    }
+
     setState((old) => ({ ...old, isLoading: true }));
     saveSurveyResult
       .save({ answer })
